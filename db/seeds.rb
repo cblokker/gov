00bda3c31346f8206abee10b57e654f8926747d2
Chase Blokker <chaseblokker@yahoo.com>
2014-09-05 17:24:29 -0400

    cleaned up spacing in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,16 +15,18 @@ LegislationsHelper.search_by_date_fedreg('2014-06-01')['results'].each do |resul
 	Legislation.create(
 	  title: result['title'],
 	  publication_date: result['publication_date'],
-      abstract: result['abstract'],
-      url: result['html_url'])
+    abstract: result['abstract'],
+    url: result['html_url']
+  )
 end
 
 LegislationsHelper.search_by_date_govtrack('2011-01-05')['objects'].each do |object|
 	Legislation.create(
 	  title: object['title_without_number'],
 	  publication_date: object['introduced_date'],
-      abstract: object['titles'][0][2],
-      url: object['thomas_link'])
+    abstract: object['titles'][0][2],
+    url: object['thomas_link']
+  )
 end
 
 # keywords_to_fetch = Legistation.take(10)
@@ -36,10 +38,12 @@ Legislation.all.each do |leg|
 			first_paragraph: article['lead_paragraph'],
 			publication_date: article['pub_date'],
 			url: article['web_url'],
-			source: 'New York Times')
+			source: 'New York Times'
+		)
 		ArticlesLegislation.create(
 			article: a,
-			legislation: leg)
+			legislation: leg
+		)
 	end
 end
 
